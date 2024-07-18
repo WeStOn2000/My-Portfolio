49a77888a53325115a6946bdcab80b21c88c6663
WeStOn2000 <161762685+WeStOn2000@users.noreply.github.com>
2024-07-18 17:26:37 -0400

    .....

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
+/*
+*   Setup for the Express app
+*/
+// Imports necessary modules
 const express = require("express");
 const path = require('path');
 const app = express();
 const  data  = require("./data.json");
-
+// view engine to Pug and Serves static files from the public directory
 app.set("view engine", "pug");
 app.use('/static',express.static(path.join(__dirname,'public')));
-
+/*
+*   Route Handlers
+*/
+// Route for the homepage
 app.get("/", (req, res) => {
     res.render("index", { projects: data.projects }); 
   });
-
+// Route for the about page
 app.get("/about", (req,res) => {
  res.render('about');
 });
-
+// Route for displaying project details
 app.get('/projects/:id', (req, res) => {
     const projectId = req.params.id;
     const project = data.projects.find(p => p.id == projectId);
@@ -24,13 +31,16 @@ app.get('/projects/:id', (req, res) => {
       res.status(404).send('Project not found');
     }
   });
-
+/*
+*   Error Handlers
+*/
+// 404 Handler
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
-  
+  // Global Error Handler
   app.use((err, req, res, next) => {
     res.status(err.status|| 500);
     res.render('error',{
@@ -38,7 +48,9 @@ app.use((req, res, next) => {
      error: {}
     });
   });
-
+/*
+*   Starting the server
+*/
 app.listen(3000,() => {
     console.log("Port:3000 Running");
 });
